c0e60c866f5a8e6338f90a4d38b016d72d95c159
lilchix <104845593+lilchix@users.noreply.github.com>
2023-06-15 11:20:38 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@ function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
-  
+}
+
 function aggiungiAlCarrello() {
   var carrello = document.getElementById("carrello");
   var numero = parseInt(carrello.textContent);
